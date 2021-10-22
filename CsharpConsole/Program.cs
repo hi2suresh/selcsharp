00fb9dc56dc04d807cfce6861a371952c9bf43d4
hi2suresh <hi2suresh@gmail.com>
2021-10-23 05:49:08 +0800

    Sample linq code

--- CsharpConsole/Program.cs
+++ CsharpConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CsharpConsole
 {
@@ -6,7 +8,33 @@ namespace CsharpConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //var numbers = new List<int> {10,20,50,60,15,29 };
+            //numbers.ForEach(num => Console.WriteLine(num));
+
+            var cities = new List<City> { new City(1, "Delhi"), new City(2, "Perth"), new City(3, "Brisbane"), new City(4, "Sydney"), new City(5, "Bangalore") };
+
+            var results = from c in cities
+                          where c.Name.StartsWith("B")
+                          select c;
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item.Id + " : " + item.Name);
+            }
+
         }
+    }
+
+    class City
+    {
+        public City(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
